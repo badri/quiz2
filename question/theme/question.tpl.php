a8af99f8368f7aa55812fe1a162ecb0e76dab6dc
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2014-08-10 21:57:03 +0530

    more hooks added to question module

--- question/theme/question.tpl.php
+++ question/theme/question.tpl.php
@@ -2,22 +2,22 @@
 
 /**
  * @file
- * Default theme implementation for entities.
+ * A basic template for question entities
  *
  * Available variables:
  * - $content: An array of comment items. Use render($content) to print them all, or
  *   print a subset such as render($content['field_example']). Use
  *   hide($content['field_example']) to temporarily suppress the printing of a
  *   given element.
- * - $title: The (sanitized) entity label.
- * - $url: Direct url of the current entity if specified.
- * - $page: Flag for the full page state.
+ * - $title: The name of the question
+ * - $url: The standard URL for viewing a question entity
+ * - $page: TRUE if this is the main view page $url points too.
  * - $classes: String of classes that can be used to style contextually through
  *   CSS. It can be manipulated through the variable $classes_array from
  *   preprocess functions. By default the following classes are available, where
  *   the parts enclosed by {} are replaced by the appropriate values:
- *   - entity-{ENTITY_TYPE}
- *   - {ENTITY_TYPE}-{BUNDLE}
+ *   - entity-profile
+ *   - question-{TYPE}
  *
  * Other variables:
  * - $classes_array: Array of html class attribute values. It is flattened
@@ -29,6 +29,12 @@
  */
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>>
+        <a href="<?php print $url; ?>"><?php print $title; ?></a>
+    </h2>
+  <?php endif; ?>
+
   <div class="content"<?php print $content_attributes; ?>>
     <?php
       print render($content);
